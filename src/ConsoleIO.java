c77a7bcb6cbbec8836f88443ca08078aceb0c4c6
Anthony J Hernandez <hernandez.jg.anthony@gmail.com>
2023-06-15 09:26:51 -0500

    ConsoleIO

---
+++ src/ConsoleIO.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ConsoleIO {
+    public static void main(String[] args) {
+
+//        String name = "codeup";
+//        System.out.println(name);
+//        System.out.print(name);
+//        System.out.print(name);
+//        System.out.print(name);
+
+//        System.out.printf("Hello there, %s. Nice to see you.%n", name);
+//
+//        System.out.println("Hello there, " + name + " Nice to see you.");
+
+//        String greeting = "Salutations";
+//        String name = "codeup";
+//        System.out.printf("%s, %s! %n", greeting, name);
+//        System.out.printf("%s, %s!",name , greeting);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+        System.out.print("Enter something again: ");
+        String userInput2 = scanner.nextLine();
+
+        System.out.println("You entered: --> \"" + userInput2 + "\" <--");
+
+
+    }
+}
