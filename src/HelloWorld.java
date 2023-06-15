7242dd2dc284c0d10c84b30c23f70268e353f2d3
Anthony J Hernandez <hernandez.jg.anthony@gmail.com>
2023-06-15 09:15:00 -0500

    Completing missed work from yesterday

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,73 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.println("World!");
+
+        int myFavoriteNumber = 42;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hello World";
+
+        System.out.println("myString = " + myString);
+
+//        float myNumber = (float) 3.14;
+
+//        float myNumber = 3.14F;
+
+//        double myNumber = 3.14;
+
+//        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        int theNumberThree = 3;
+//        Object o = theNumberThree;
+//        System.out.println("o = " + o);
+//        int three = (int) o;
+
+//        System.out.println("three = " + three);
+
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//
+//        System.out.println("x = " + x);
+
+//        int x = 3;
+//        int y = 4;
+
+//        y = y * x;
+
+//        y *= x;
+//
+//        System.out.println("y = " + y);
+
+        int x = 10;
+        int y = 2;
+
+//        x = x / y;
+//        x /= y;
+//        System.out.println(x);
+
+//        y = y - x;
+
+//        y -= x;
+//
+//        System.out.println(y);
+
+        byte myByte = 127;
+        System.out.println("myByte = " + myByte);
+
+        myByte++;
+
+        System.out.println("myByte = " + myByte);
+
     }
 }
+
---
+++ src/SyntaxTypesNotes.java
@@ -0,0 +1,27 @@
+public class SyntaxTypesNotes {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        int id;
+        boolean isLoggedIn;
+        float bankAccountBalance;
+
+        int theAnswer = 42;
+        String question = "What is your favorite color?";
+
+//        .sout
+        System.out.println(question);
+//        .soutv
+        System.out.println("theAnswer = " + theAnswer);
+
+        double pi = 3.14159;
+        System.out.println("pi = " + pi);
+        int almostPi = (int) pi;
+        System.out.println("almostPi = " + almostPi);
+
+
+
+
+    }
+}
